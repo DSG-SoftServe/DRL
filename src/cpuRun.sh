0ba563e886d6d460992d23a571bf607edabd9cc2
spaceuniverse <neverdraw@gmail.com>
2015-04-03 19:49:28 +0300

    Global refactoring

--- src/cpuRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-time THEANO_FLAGS=mode=FAST_RUN,floatX=float32,device=cpu python $*
-
--- src/gpuRun.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-time THEANO_FLAGS=cuda.root=/usr/local/cuda-6.5,mode=FAST_RUN,floatX=float32,device=gpu python $*
-
