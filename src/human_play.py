3e130c1ec8c4e98634c534afd0c31c8b2fe09dcb
spaceuniverse <neverdraw@gmail.com>
2015-04-08 15:42:54 +0300

    statistics collecting class for human playing

--- src/human_play.py
+++ src/human_play.py
@@ -1,17 +1,46 @@
 #-----------------------------------------------#
-# Class for human demo.
+# Class for human demo. Linux only for now.
+# For windows should implement:
+# getkey() <-> from msvcrt import getch
+# Controls: w a s d
 #-----------------------------------------------#
 from ale.ale import ale
 from memory.memory import memory
 from ai.neuralnet import neuralnet
 import time
-import Tkinter as tk
+import termios
+import sys
+import os
+import threading
 #-----------------------------------------------#
 
-def keypress(event):
-    if event.keysym == 'Escape':
-        root.destroy()
-    print event.char
+TERMIOS = termios
+
+def getkey():
+    fd = sys.stdin.fileno()
+    old = termios.tcgetattr(fd)
+    new = termios.tcgetattr(fd)
+    new[3] = new[3] & ~TERMIOS.ICANON & ~TERMIOS.ECHO
+    new[6][TERMIOS.VMIN] = 1
+    new[6][TERMIOS.VTIME] = 0
+    termios.tcsetattr(fd, TERMIOS.TCSANOW, new)
+    c = None
+    try:
+        c = os.read(fd, 1)
+    finally:
+        termios.tcsetattr(fd, TERMIOS.TCSAFLUSH, old)
+    return c
+
+char = None
+
+def keypress():
+    global char
+    char = getkey()
+
+# Start thread for keypress
+t = threading.Thread(target=keypress)
+t.daemon = True
+t.start()
 
 class play:
     def __init__(self):
@@ -22,26 +51,43 @@ class play:
         self.ale = ale(self.memory, frames_to_skip=6)
 
     def run_game(self):
+        global char, t
         while True:
             self.ale.start_game()
             while not self.ale.game_over:
                 # Delay for slow human perception
-                time.sleep(0.05)
+                time.sleep(0.1)   # NN plays without delay. Can you?
                 # For game start we should press fire
-                action = 1
+                #-----------------------
+                # Debug
+                #print "Key", char
+                if not(t.isAlive()):
+                    t = threading.Thread(target=keypress)
+                    t.daemon = True
+                    t.start()
+                #-----------------------
+                if str(char) == "d":
+                    action = 2
+                elif str(char) == "a":
+                    action = 3
+                elif str(char) == "w":
+                    action = 1
+                else:
+                    action = 0
+                print '\naction HUMAN:', action
+
+                # We collect all moves, so using special methods
+                # we can save them to use for training process later:
+                # self.memory.mem_save(self.steps)
+
                 # Act and store action in memory
                 self.ale.move(action)
+                # Clearing key
+                char = None
             self.ale.finish_game()
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    print "Press a key (Escape key to exit):"
-    root.bind_all('<Key>', keypress)
-    # don't show the tk window
-    root.withdraw()
-    root.mainloop()
     P = play()
     P.run_game()
-
 
 #-----------------------------------------------#
