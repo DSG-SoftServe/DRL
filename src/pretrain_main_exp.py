8e6bd7d24082bc87cd88dfdb258536191739c69b
spaceuniverse <neverdraw@gmail.com>
2015-06-09 18:37:16 +0300

    pretrain script fixes and mapper + mapper to fix bug in manual collected data

--- src/pretrain_main_exp.py
+++ src/pretrain_main_exp.py
@@ -21,19 +21,36 @@ class main:
         self.memory.D = cPickle.load(f)
         f.close()
 
+    def __mapper__(self, batch):
+        # -------
+        # 0 1 2 3 Our
+        # 0 1 3 4
+        # -------
+        # 0 1 2 3 4 5 Error Map
+        # 0 1 3 2 5 4
+        # -------
+        for b in batch:
+            print b['action'],
+            if b['action'] == 3 or b['action'] == 4:
+                b['action'] = 0
+            if b['action'] == 5:
+                b['action'] = 3
+
     def run_game(self):
         # Maximum number of episodes
         while self.steps < 700000:
             # Measurement of time for iteration
             timeStart = time.time()
-            self.n_net.train(self.memory.get_minibatch(self.minibatch_size))
+            btch = self.memory.get_minibatch(self.minibatch_size)
+            self.__mapper__(btch)   # Only if there are mistakes in ale.py self.actions_list
+            self.n_net.train(btch)
             self.steps += 1
             timeStop = time.time()
             print "Step:", self.steps
             # Time used for iteration in minutes
             print round((timeStop - timeStart) / 60, 5)
             # Saving model
-            if self.steps % 30000:
+            if self.steps % 30000 == 0:
                 self.n_net.saveModel('./models/AS.model.weights.NN.NMem.' + str(self.steps))
             # Sum of weights of hidden layers for debug
             self.n_net.printedW()
